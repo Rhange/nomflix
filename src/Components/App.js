c3ff7191c7ff83d93b4a6f924e16dbca05652843
Rhange <rhange0511@gmail.com>
2020-04-09 13:13:21 +0900

    #3.2 CSS in React part 2
    1. index.js는 App.js에서 Header를 찾을 때, 경로를 간단하게 하여 추적할 수 있게 해줌.
    
    2. css 파일을 Header.module.css처럼 사용하면 모듈처럼(import styles from "./Header.module.css";) 사용가능. css에 입력한 코드에서 selector를 property처럼(className={styles.navList}) 사용할 수 있음.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Router from "./Router";
-import Header from "../Components/Header";
+import Router from "Components/Router";
+import Header from "Components/Header";
 
 function App() {
   return (
