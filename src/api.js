f6dec30fde0217a0f7af56ac58dc8dab12ace5e5
Rhange <rhange0511@gmail.com>
2020-04-10 15:41:54 +0900

    #4.2 API Verbs part One
    - added movieApi and tvApi by using implicit arrow function and api.get( ).

--- src/api.js
+++ src/api.js
@@ -11,7 +11,14 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-/* "/tv/popular"가 아닌 "tv/popular"로 쓴다. 'baseURL' 맨 뒤에 '/'가 존재함. '/'를 앞에 써주는 것은 절대경로를 의미 */
-api.get("tv/popular");
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular")
+};
 
-export default api;
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today")
+};
