c0276c3d987579abafd8a82b21932c46e3ed933d
Rhange <rhange0511@gmail.com>
2020-04-11 13:35:36 +0900

    Update Router.js

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -5,9 +5,9 @@ import {
   Redirect,
   Switch
 } from "react-router-dom";
-import Home from "../Routes/Home";
-import TV from "../Routes/TV";
-import Search from "../Routes/Search";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
 import Header from "./Header";
 
 export default () => (
@@ -15,7 +15,7 @@ export default () => (
     <Header />
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
+      <Route path="/tv" component={TV} />
       <Route path="/search" component={Search} />
       <Redirect from="*" to="/" />
     </Switch>
