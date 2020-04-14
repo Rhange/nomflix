6eb866ebc0d9048c2a634496d507c13d9a5645f4
Rhange <rhange0511@gmail.com>
2020-04-14 15:26:02 +0900

    #6.6 Rendering Poster Component
    - rendering Poster component according to movie API info & show API info
    
    - attach completed Poster component to SearchPresenter component

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "Components/Section";
-import Loader from "../../Components/Loader";
-import Message from "../../Components/Message";
-import Poster from "../../Components/Poster";
+import Loader from "Components/Loader";
+import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -18,21 +18,45 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) =>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map((movie) => (
-            <Poster />
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              imageUrl={movie.poster_path}
+              rating={movie.vote_average}
+              isMovie={true}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+            />
           ))}
         </Section>
       )}
       {upcoming && upcoming.length > 0 && (
         <Section title="Upcoming Movies">
           {upcoming.map((movie) => (
-            <Poster />
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              imageUrl={movie.poster_path}
+              rating={movie.vote_average}
+              isMovie={true}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+            />
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Movies">
           {popular.map((movie) => (
-            <Poster />
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              imageUrl={movie.poster_path}
+              rating={movie.vote_average}
+              isMovie={true}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+            />
           ))}
         </Section>
       )}
