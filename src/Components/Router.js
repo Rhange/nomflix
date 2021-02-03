ec363e008f561ff1aa44a0e6c7fdaa5c246a0fae
Rhange <rhange0511@gmail.com>
2021-02-03 22:21:41 +0900

    .env NODE_PATH 방법이 deprecated 되어서 jsconfig.json에 설정하는 것으로 수정

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
 } from "react-router-dom";
 import Header from "./Header";
 import Detail from "Routes/Detail";
@@ -12,15 +12,15 @@ import Search from "Routes/Search";
 import TV from "Routes/TV";
 
 export default () => (
-  <Router>
-    <Header />
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" component={TV} />
-      <Route path="/search" component={Search} />
-      <Route path="/movie/:id" component={Detail} />
-      <Route path="/show/:id" component={Detail} />
-      <Redirect from="*" to="/" />
-    </Switch>
-  </Router>
+	<Router>
+		<Header />
+		<Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/tv" component={TV} />
+			<Route path="/search" component={Search} />
+			<Route path="/movie/:id" component={Detail} />
+			<Route path="/show/:id" component={Detail} />
+			<Redirect from="*" to="/" />
+		</Switch>
+	</Router>
 );
