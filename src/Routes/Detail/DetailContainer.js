dce2bf0238ee6c04edaf8eeb2918d5e65c3c9f8d
rhange <rhange0511@gmail.com>
2021-02-03 20:55:06 +0900

    Updated

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,59 +1,59 @@
-import React from "react";
-import DetailPresenter from "./DetailPresenter";
-import { tvApi, movieApi } from "api";
+import React from 'react';
+import DetailPresenter from './DetailPresenter';
+import { tvApi, movieApi } from 'api';
 
 export default class extends React.Component {
-  constructor(props) {
-    super(props);
-    const {
-      location: { pathname }
-    } = props;
-    this.state = {
-      result: null,
-      loading: true,
-      error: null,
-      isMovie: pathname.includes("/movie/")
-    };
-  }
+	constructor(props) {
+		super(props);
+		const {
+			location: { pathname },
+		} = props;
+		this.state = {
+			result: null,
+			loading: true,
+			error: null,
+			isMovie: pathname.includes('/movie/'),
+		};
+	}
 
-  async componentDidMount() {
-    const {
-      match: {
-        params: { id }
-      },
-      history: { push }
-    } = this.props;
+	async componentDidMount() {
+		const {
+			match: {
+				params: { id },
+			},
+			history: { push },
+		} = this.props;
 
-    const { isMovie } = this.state;
+		const { isMovie } = this.state;
 
-    const parsedId = parseInt(id);
-    if (isNaN(parsedId)) {
-      return push("/");
-    }
+		const parsedId = parseInt(id);
+		if (isNaN(parsedId)) {
+			return push('/');
+		}
 
-    let result = null;
-    try {
-      if (isMovie) {
-        ({ data: result } = await movieApi.movieDetail(parsedId));
-      } else {
-        ({ data: result } = await tvApi.showDetail(parsedId));
-      }
-      console.log(result);
-    } catch {
-      this.setState({
-        error: "Can't find anything."
-      });
-    } finally {
-      this.setState({
-        loading: false,
-        result
-      });
-    }
-  }
+		let result = null;
+		try {
+			if (isMovie) {
+				({ data: result } = await movieApi.movieDetail(parsedId));
+			} else {
+				({ data: result } = await tvApi.showDetail(parsedId));
+			}
+			console.log(result);
+		} catch {
+			this.setState({
+				error: "Can't find anything.",
+			});
+		} finally {
+			this.setState({
+				loading: false,
+				result,
+			});
+		}
+	}
 
-  render() {
-    console.log(this.state);
-    const { result, loading, error } = this.state;
-    return <DetailPresenter result={result} loading={loading} error={error} />;
-  }
+	render() {
+		console.log(this.state);
+		const { result, loading, error } = this.state;
+		return <DetailPresenter result={result} loading={loading} error={error} />;
+	}
 }
